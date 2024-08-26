718af807b58682f6bad9da929ec3f25957161c52
guetmatt <matthias.guetebier@rub.de>
2024-08-26 17:32:29 +0200

    upload data for training and testing

---
+++ dataset_cars_bikes/car/car/resize_images.py
@@ -0,0 +1,27 @@
+#%%
+import numpy as np
+import cv2
+import os
+
+#%%
+dir_path=os.getcwd()
+print(dir_path)
+
+#%%
+width=112
+height=112
+dim=(width,height)
+
+#%%
+def main():
+    for filename in os.listdir(dir_path):
+        if filename.endswith(".jpg") or filename.endswith("JPG") or filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".JPEG"):
+            image=cv2.imread(filename)
+            print(filename)
+            resized=cv2.resize(image,dim,interpolation=cv2.INTER_AREA)
+            cv2.imwrite(filename,resized)
+
+#%%
+if __name__ == "__main__":
+    main()
+# %%
